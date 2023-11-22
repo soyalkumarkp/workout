e39ebad0c032e9382948fc95db460b5b294e11f6
soyakkumarkp <soyalkumarkp@gmail.com>
2023-11-22 23:20:23 +0530

    final

--- frontend/src/components/Workoutdetails.jsx
+++ frontend/src/components/Workoutdetails.jsx
@@ -1,4 +1,5 @@
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 const Workoutdetails = ({ workout }) => {
   const { dispatch } = useWorkoutsContext();
@@ -13,6 +14,13 @@ const Workoutdetails = ({ workout }) => {
       dispatch({ type: "DELETE_WORKOUT", payload: json });
     }
   };
+
+  // Check if workout.createdAt is a valid date before using formatDistanceToNow
+  const createdAtDate = new Date(workout.createdAt);
+  const distanceToNow =
+    isValidDate(createdAtDate) &&
+    formatDistanceToNow(createdAtDate, { addSuffix: true });
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -25,9 +33,15 @@ const Workoutdetails = ({ workout }) => {
         {workout.reps}
       </p>
       <p>{workout.createdAt}</p>
-      <span onClick={handleClick}>delete</span>
+      <p>{distanceToNow}</p>
+      <span className="material-symbols-outlined" onClick={handleClick}>
+        delete
+      </span>
     </div>
   );
 };
 
 export default Workoutdetails;
+
+// Function to check if a date is valid
+const isValidDate = (date) => !isNaN(date.getTime());
