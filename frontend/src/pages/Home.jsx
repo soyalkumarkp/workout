6704fbc9633f5cea1f2353e164dc9897265c4082
soyakkumarkp <soyalkumarkp@gmail.com>
2023-11-22 17:25:26 +0530

    Fetching Data

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
+import Workoutdetails from "../components/Workoutdetails";
 const Home = () => {
-  return <div>Home</div>;
+  const [workouts, setWorkouts] = useState(null);
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const data = await response.json();
+
+      if (response.ok) {
+        setWorkouts(data);
+      }
+    };
+    fetchWorkouts();
+  }, []);
+  return (
+    <div className="workouts">
+      {workouts &&
+        workouts.map((workout) => (
+          <Workoutdetails key={workout._id} workout={workout} />
+        ))}
+    </div>
+  );
 };
 
 export default Home;
