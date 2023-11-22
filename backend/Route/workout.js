1c7afe3d90eb35ad6198ea9369f9531e4e714db4
soyakkumarkp <soyalkumarkp@gmail.com>
2023-11-22 11:56:59 +0530

    models and schema is done

--- backend/Route/workout.js
+++ backend/Route/workout.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Workout = require("../models/workoutmodel");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -8,9 +9,19 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   res.json({ mssg: "GET a single workout" });
 });
-//post a new workout
-router.post("/", (req, res) => {
-  res.json({ mssg: "post New workout created!" });
+
+// POST a new workout
+router.post("/", async (req, res) => {
+  const { title, load, reps } = req.body;
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    // You might want to send the created workout as a response
+    res.json({ mssg: "New workout created!", workout });
+  } catch (error) {
+    // Handle errors appropriately
+    console.error(error);
+    res.status(400).json({ error: error.message });
+  }
 });
 //delete a new workout
 router.delete("/:id", (req, res) => {
